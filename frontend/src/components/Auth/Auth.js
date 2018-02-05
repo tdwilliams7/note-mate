13fa658a6024d56ae07d5dc9182a2956f578b9da
Troy Williams <troyDonwilliams@gmail.com>
2018-02-05 15:54:17 -0600

    dumb authenication working. basically anything over 4 for userbname and password works

--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 
 class Auth extends Component {
-
+  state = {
+    authenticated: false,
+  }
   checkAuthorization = (event) => {
     event.preventDefault();
     let flag = false;
     if (this.state.username.length > 4 && this.state.password.length > 4) {
-      flag = true;
-    }
-    if (flag) {
-      <Redirect to={'/notes'} />
+      this.setState({
+        authenticated: true,
+      })
     }
   }
 
   render() {
+    if (this.state.authenticated) {
+      return (
+        <Redirect to='/notes' />
+      )
+    }
     return (
       <div>
         <h1>Auth component</h1>
@@ -25,7 +31,9 @@ class Auth extends Component {
           <div>
             <input placeholder='password'></input>
           </div>
-          <button onSubmit={this.checkAuthorization}>Log In</button>
+          <Link to={"/notes"}>
+            <button >Log In</button>
+          </Link>
         </form>
       </div>
     )
